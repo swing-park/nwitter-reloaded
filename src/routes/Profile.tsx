4276bfc5df45eb87a9da1a0d095e5b9d7ec91f13
swing-park <ongsim123@gmail.com>
2023-08-23 10:30:53 +0900

    :sparkles: Feat : add profile
    - show user profile image
    - can change profile image

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,5 +1,86 @@
+import styled from "styled-components";
+import { auth, storage } from "../firebase";
+import { useState } from "react";
+import { ReactComponent as ProfileSvg } from "../assets/profile.svg";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+const AvatarUpload = styled.label`
+  width: 80px;
+  overflow: hidden;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #1d9bf0;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  svg {
+    width: 50px;
+  }
+`;
+
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+
+const AvatarInput = styled.input`
+  display: none;
+`;
+
+const Name = styled.span`
+  font-size: 22px;
+`;
+
 const Profile = () => {
-  return <h1>Profile</h1>;
+  const user = auth.currentUser;
+  const [avatar, setAvatar] = useState(user?.photoURL);
+
+  const handleOnChangeAvatar = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (!user) return;
+
+    const { files } = e.target;
+
+    if (files && files.length === 1) {
+      const file = files[0];
+
+      if (file.size > 1024 ** 2) return;
+
+      const locationRef = ref(storage, `avatars/${user.uid}`);
+      const ret = await uploadBytes(locationRef, file);
+      const avatarUrl = await getDownloadURL(ret.ref);
+
+      setAvatar(avatarUrl);
+
+      await updateProfile(user, {
+        photoURL: avatarUrl,
+      });
+    }
+  };
+
+  return (
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {avatar ? <AvatarImg src={avatar} /> : <ProfileSvg />}
+      </AvatarUpload>
+      <AvatarInput
+        onChange={handleOnChangeAvatar}
+        id="avatar"
+        type="file"
+        accept="image/*"
+      />
+      <Name>{user?.displayName ? user.displayName : "Anonymous"}</Name>
+    </Wrapper>
+  );
 };
 
 export default Profile;
